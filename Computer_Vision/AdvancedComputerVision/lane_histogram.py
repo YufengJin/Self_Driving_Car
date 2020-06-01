d2ede3c0a8c096401fa655756b82c21aa170ea9a
YufengJin <jyf931118@gmail.com>
2020-06-01 13:21:17 +0200

    add advanced computer vision and fundmental knowledge of Neural network

---
+++ Computer_Vision/AdvancedComputerVision/lane_histogram.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+# Load our image
+# `mpimg.imread` will load .jpg as 0-255, so normalize back to 0-1
+img = mpimg.imread('warped-example.jpg')/255
+
+def make_histogram(img,orient):
+    histogram_x = np.zeros(img.shape[1])
+    histogram_y = np.zeros(img.shape[0])
+    if orient == 'x':
+        for i in range(img.shape[1]):
+            histogram_x[i] = sum(img[:,i])
+        return histogram_x
+    elif orient == 'y':
+        for i in range(img.shape[0]):
+            histogram_y[i] = sum(img[i,:])
+        return histogram_y
+    else:
+        raise ValueError('Please check the defined orient of image.')
+
+def hist(img):
+    # TO-DO: Grab only the bottom half of the image
+    # Lane lines are likely to be mostly vertical nearest to the car
+    bottom_half = img[img.shape[0]//2:,:]
+    # TO-DO: Sum across image pixels vertically - make sure to set `axis`
+    # i.e. the highest areas of vertical lines should be larger values
+    histogram = np.sum(bottom_half, axis=0)
+    #histogram = make_histogram(bottom_half,'g')
+
+    return histogram
+
+# Create histogram of image binary activations
+histogram = hist(img)
+
+# Visualize the resulting histogram
+plt.plot(histogram)
+plt.show()
